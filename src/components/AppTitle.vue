fddfdac516380e8b8f2430f2fcca6f8ca0074b59
martinaapalazzi <martinaapalazzi@gmail.com>
2024-01-17 15:45:50 +0100

    Esercizio finito

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,27 @@
+<script>
+
+    export default {
+        data() {
+            return { title: 'La mia prima app con Vite!' }
+        },
+    }
+
+</script>
+
+<template>
+
+    <div>
+        <h1 class="main-title">
+            {{ title }}
+        </h1>
+    </div>
+  
+</template>
+
+<style scoped>
+
+.main-title{
+    color: red;
+}
+
+</style>
